6c93c1a1021a410138d21c4ef88010b8e64dbd96
Zeynep Hanife Çelikoğlu <zeynephcelikoglu@icloud.com>
2024-07-18 23:00:29 +0300

    Add project files.

---
+++ HelloWebApi/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelloWebApi.Models;
+
+namespace HelloWebApi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage() 
+        {
+            var result = new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello ASP .NET Core Web API"
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            this.logger=logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id=1, ProductName = "Computer"},
+                new Product() {Id=2, ProductName = "Keyboard"},
+                new Product() {Id=3, ProductName = "Mouse"},
+
+            };
+            this.logger.LogInformation("GetAllProducts action has been called.");
+            return Ok(products);
+        }
+
+
+
+
+    }
+}
